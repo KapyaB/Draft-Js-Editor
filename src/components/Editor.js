68fc7813a91be20bd4092692994e8d328b7b4eaa
Bright <kapyabright@outlook.com>
2019-10-09 23:57:22 +0200

    more custome styling: strikethrough, highlight, super and sub scripts

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -49,7 +49,49 @@ const EditorComp = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'CODE'));
   };
 
+  // STRIKE THROUGH
+  const onStrikeThroughClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'STRIKETHROUGH'));
+  };
+
+  // HIGHLIGHT
+  const onHighlightClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'HIGHLIGHT'));
+  };
+
+  // SUPERSCRIPT
+  const onSuperScriptClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'SUPERSCRIPT'));
+  };
+
+  // SUBSCRIPT
+  const onSubScriptClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'SUBSCRIPT'));
+  };
+
   // custom inline styles
+  const styleMap = {
+    STRIKETHROUGH: {
+      textDecoration: 'line-through'
+    },
+    CODE: {
+      color: '#00398a',
+      background: '#ccc8',
+      fontFamily: 'Inconsolata, monospace',
+      fontWeight: 'bold'
+    },
+    HIGHLIGHT: {
+      background: '#e9ff32'
+    },
+    SUPERSCRIPT: {
+      verticalAlign: 'super',
+      fontSize: 'smaller'
+    },
+    SUBSCRIPT: {
+      verticalAlign: 'sub',
+      fontSize: 'smaller'
+    }
+  };
 
   return (
     <div className="editor-wrapper">
@@ -58,9 +100,11 @@ const EditorComp = () => {
           <button className="bold-btn" onClick={onBoldclick}>
             <strong>B</strong>
           </button>
+
           <button className="italic-btn" onClick={onItalicClick}>
             <em>I</em>
           </button>
+
           <button
             className="underline-btn"
             style={{ textDecoration: 'underline' }}
@@ -68,14 +112,42 @@ const EditorComp = () => {
           >
             U
           </button>
+
           <button className="code-btn" onClick={onCodeClick}>
             {'<>'}
+          </button>
+
+          <button
+            className="strike-through-btn"
+            style={{ textDecoration: 'line-through' }}
+            onClick={onStrikeThroughClick}
+          >
+            ab
+          </button>
+
+          <button
+            className="highlight-btn"
+            style={{
+              borderBottom: '4px solid #e9ff32'
+            }}
+            onClick={onHighlightClick}
+          >
+            <i className="fas fa-highlighter"></i>
+          </button>
+
+          <button className="superscript-btn" onClick={onSuperScriptClick}>
+            X<sup>2</sup>
+          </button>
+
+          <button className="subscript-btn" onClick={onSubScriptClick}>
+            X<sub>2</sub>
           </button>
         </div>
       </div>
       <Editor
         editorState={editorState}
         handleKeyCommand={handleKeyCommand}
+        customStyleMap={styleMap}
         onChange={setEditorState}
       />
     </div>
