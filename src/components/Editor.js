addcd6557f6308b95862775776d54e2ac6828968
Bright <kapyabright@outlook.com>
2019-10-10 15:54:30 +0200

    tool bar styling

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { EditorState, RichUtils } from 'draft-js';
-import Editor, { composeDecorators } from 'draft-js-plugins-editor';
+import Editor from 'draft-js-plugins-editor';
 import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
 import createLinkPlugin from 'draft-js-anchor-plugin';
-import { ItalicButton, BoldButton, UnderlineButton } from 'draft-js-buttons';
+import createUndoPlugin from 'draft-js-undo-plugin';
+import createImagePlugin from 'draft-js-image-plugin';
 
-import { mediaBlockRenderer } from './entities/mediaBlockRenderer';
+import { ItalicButton, BoldButton, UnderlineButton } from 'draft-js-buttons';
 
 import 'draft-js/dist/Draft.css';
 import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
@@ -14,6 +15,7 @@ import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
 const linkPlugin = createLinkPlugin({
   placeholder: 'https://...'
 });
+
 // INLINE TOOLBAR
 //  At this step, a configuration object can be passed in as an argument.
 const inlineToolbarPlugin = createInlineToolbarPlugin({
@@ -21,8 +23,24 @@ const inlineToolbarPlugin = createInlineToolbarPlugin({
 });
 const { InlineToolbar } = inlineToolbarPlugin;
 
+// UNDO REDO
+const theme = {
+  undo: 'style-btn',
+  redo: 'style-btn'
+};
+
+const undoPlugin = createUndoPlugin({
+  undoContent: 'Undo',
+  redoContent: 'Redo',
+  theme
+});
+const { UndoButton, RedoButton } = undoPlugin;
+
+// IMAGE
+const imagePlugin = createImagePlugin();
+
 // plugins list
-const plugins = [inlineToolbarPlugin, linkPlugin];
+const plugins = [inlineToolbarPlugin, linkPlugin, undoPlugin, imagePlugin];
 
 const EditorComp = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -48,9 +66,20 @@ const EditorComp = () => {
 
   //  Styling controls
 
+  // keep track of current style
+  const [activeBtns, setActiveBtns] = useState([]);
+
   // BOLD
   const onBoldclick = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+    console.log(editorState);
+    if (activeBtns.indexOf('bold') !== -1) {
+      /* remove style */
+      setActiveBtns(activeBtns.filter(btn => btn !== 'bold'));
+    } else {
+      /* add style */
+      setActiveBtns([...activeBtns, 'bold']);
+    }
   };
 
   // ITALICS
@@ -142,14 +171,10 @@ const EditorComp = () => {
     );
   };
 
+  // LIST LEVEL
   const onTab = e => {
     const maxDepth = 5;
     setEditorState(RichUtils.onTab(e, editorState, maxDepth));
-  };
-
-  // IMAGE
-  const onImageClick = () => {
-    setEditorState(RichUtils.toggleBlockType(editorState, 'atomic'));
   };
 
   // custom inline styles
@@ -185,29 +210,38 @@ const EditorComp = () => {
   return (
     <div className="editor-wrapper">
       <div className="editor-tools">
+        <div className="undo-redo">
+          <UndoButton />
+          <RedoButton />
+        </div>
         <div className="inline-styles">
-          <button className="bold-btn" onClick={onBoldclick}>
+          <button
+            className={`bold-btn style-btn ${
+              activeBtns.includes('bold') ? 'active-btn' : ''
+            }`}
+            onClick={onBoldclick}
+          >
             <strong>B</strong>
           </button>
 
-          <button className="italic-btn" onClick={onItalicClick}>
+          <button className="style-btn italic-btn" onClick={onItalicClick}>
             <em>I</em>
           </button>
 
           <button
-            className="underline-btn"
+            className="style-btn underline-btn"
             style={{ textDecoration: 'underline' }}
             onClick={onUnderlineClick}
           >
             U
           </button>
 
-          <button className="code-btn" onClick={onCodeClick}>
+          <button className="style-btn code-btn" onClick={onCodeClick}>
             {'<>'}
           </button>
 
           <button
-            className="strike-through-btn"
+            className="style-btn strike-through-btn"
             style={{ textDecoration: 'line-through' }}
             onClick={onStrikeThroughClick}
           >
@@ -215,86 +249,92 @@ const EditorComp = () => {
           </button>
 
           <button
-            className="highlight-btn"
-            style={{
-              borderBottom: '4px solid #e9ff32'
-            }}
+            className="style-btn highlight-btn"
             onClick={onHighlightClick}
           >
-            <i className="fas fa-highlighter"></i>
+            <i className=" fas fa-highlighter"></i>
           </button>
 
-          <button className="superscript-btn" onClick={onSuperScriptClick}>
+          <button
+            className="style-btn superscript-btn"
+            onClick={onSuperScriptClick}
+          >
             X<sup>2</sup>
           </button>
 
-          <button className="subscript-btn" onClick={onSubScriptClick}>
+          <button
+            className="style-btn subscript-btn"
+            onClick={onSubScriptClick}
+          >
             X<sub>2</sub>
           </button>
         </div>
         <div className="block-styles">
           <div className="headers">
-            <button className="h1-btn" onClick={onH1Click}>
+            <button className="style-btn h1-btn" onClick={onH1Click}>
               H1
             </button>
-            <button className="h1-btn" onClick={onH2Click}>
+            <button className="style-btn h1-btn" onClick={onH2Click}>
               H2
             </button>
-            <button className="h1-btn" onClick={onH3Click}>
+            <button className="style-btn h1-btn" onClick={onH3Click}>
               H3
             </button>
-            <button className="h1-btn" onClick={onH4Click}>
+            <button className="style-btn h1-btn" onClick={onH4Click}>
               H4
             </button>
-            <button className="h1-btn" onClick={onH5Click}>
+            <button className="style-btn h1-btn" onClick={onH5Click}>
               H5
             </button>
-            <button className="h1-btn" onClick={onH6Click}>
+            <button className="style-btn h1-btn" onClick={onH6Click}>
               H6
             </button>
           </div>
-          <button className="blockquote-btn" onClick={onBlockquoteClick}>
+          <button
+            className="style-btn blockquote-btn"
+            onClick={onBlockquoteClick}
+          >
             <i className="fas fa-quote-right"></i>
           </button>
-          <button className="codeblock-btn" onClick={onCodeBlockClick}>
+          <button
+            className="style-btn codeblock-btn"
+            onClick={onCodeBlockClick}
+          >
             code
           </button>
 
-          <button className="ordered-btn" onClick={onOLClick}>
+          <button className="style-btn ordered-btn" onClick={onOLClick}>
             <i className="fas fa-list-ol"></i>
           </button>
-          <button className="ordered-btn" onClick={onULClick}>
+          <button className="style-btn ordered-btn" onClick={onULClick}>
             <i className="fas fa-list"></i>
-          </button>
-
-          <button className="image-btn" onClick={onImageClick}>
-            <i className="far fa-image"></i>
           </button>
         </div>
       </div>
-      <Editor
-        editorState={editorState}
-        handleKeyCommand={handleKeyCommand}
-        customStyleMap={styleMap}
-        plugins={plugins}
-        blockStyleFn={myBlockStyleFn}
-        onTab={onTab}
-        blockRendererFn={mediaBlockRenderer}
-        onChange={setEditorState}
-        placeholder="Whats on your mind?"
-        spellCheck={true}
-      />
-      <InlineToolbar>
-        {// may be use React.Fragment instead of div to improve perfomance after React 16
-        externalProps => (
-          <div>
-            <BoldButton {...externalProps} />
-            <ItalicButton {...externalProps} />
-            <UnderlineButton {...externalProps} />
-            <linkPlugin.LinkButton {...externalProps} />
-          </div>
-        )}
-      </InlineToolbar>
+      <div className="editor-container">
+        <Editor
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          customStyleMap={styleMap}
+          plugins={plugins}
+          blockStyleFn={myBlockStyleFn}
+          onTab={onTab}
+          onChange={setEditorState}
+          placeholder="Whats on your mind?"
+          spellCheck={true}
+        />
+        <InlineToolbar>
+          {// may be use React.Fragment instead of div to improve perfomance after React 16
+          externalProps => (
+            <Fragment>
+              <BoldButton {...externalProps} />
+              <ItalicButton {...externalProps} />
+              <UnderlineButton {...externalProps} />
+              <linkPlugin.LinkButton {...externalProps} />
+            </Fragment>
+          )}
+        </InlineToolbar>
+      </div>
     </div>
   );
 };
